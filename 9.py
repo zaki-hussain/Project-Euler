51f563c331b27f340f29c2ee18d8a8cefb8b1f7e
zaki-hussain <zakriyahussain6@gmail.com>
2023-10-05 19:40:54 +0100

    Question 9

---
+++ 9.py
@@ -0,0 +1,20 @@
+#A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+#a^2 + b^2 = c^2.
+#For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+#There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+#Find the product abc.
+
+#m(m+n)=1000 where m and n are odd
+#Factors of 1000 are 2^3 * 5^3 so:
+m = 25
+n = 15
+
+#a = m * n
+#b = (m**2 - n**2) / 2
+#c = (m**2 + n**2) / 2
+
+answer = int(m * n * (m**4 - n**4) / 4)
+
+print(answer)
+
+#
