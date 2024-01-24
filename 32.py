329050bae3aa3d149ec6b674b4e9145e60b8765f
Zaki Hussain <zakriyahussain6@gmail.com>
2024-01-24 10:01:10 +0000

    Completed question 32

--- 32.py
+++ 32.py
@@ -5,3 +5,24 @@
 # Find the sum of all products whose multiplicand/multiplier/product identity can be written as a through pandigital.
 # HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
 
+products = set()
+
+def checkPandigital(all):
+    for _ in "123456789":
+        if _ not in all:
+            return False
+    return True
+
+def check(range1, range2, range3, range4):
+    for i in range(range1, range2):
+        for j in range(range3, range4):
+            n = i * j
+            if n > 9999:
+                break
+            if checkPandigital(str(i) + str(j) + str(n)):
+                products.add(n)
+
+check(1, 10, 1000, 10000)
+check(10, 100, 100, 1000)
+
+print(sum(products))
